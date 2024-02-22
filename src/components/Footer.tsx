25a246d877700258aea14dcd9eb9835d9e83e2b8
GaurangBhandare10 <gnbhandare20@gmail.com>
2024-02-22 21:21:36 +0530

    first commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className="h-12  max-[376px]:text-xs max-[376px]:h-[6px]  md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between">
       <Link href="/" className="font-bold text-xl">
-        GB's Cafe
+        GB&apos;s Cafe
       </Link>
       <p> {currenDate.getFullYear()} © ALL RIGHTS RESERVED.</p>
     </div>

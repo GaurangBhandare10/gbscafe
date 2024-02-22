3235c24f66916728bae46a7e9bceaf34604f1bad
GaurangBhandare10 <gnbhandare20@gmail.com>
2024-02-22 21:30:07 +0530

    second commit

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="h-[calc(100vh-6rem)] w-full flex items-center justify-center text-center px-4">
+      <h1>For any help/queries please drop a mail at gnbhandare20@gmail.com</h1>
+    </div>
+  );
+};
+
+export default Contact;
